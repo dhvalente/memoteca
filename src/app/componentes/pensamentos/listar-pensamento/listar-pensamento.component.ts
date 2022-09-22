b2d6acf23754b8962c9cd4a5f97900240647a754
Diogo Valente <diogovalente@hotmail.com.br>
2022-09-21 21:19:03 -0300

    Projeto Memoteca - ANGULAR

---
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -0,0 +1,23 @@
+import { Pensamento } from './../pensamentos';
+import { PensamentoService } from './../pensamento.service';
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-listar-pensamento',
+  templateUrl: './listar-pensamento.component.html',
+  styleUrls: ['./listar-pensamento.component.css']
+})
+export class ListarPensamentoComponent implements OnInit {
+
+  listaPensamentos: Pensamento[] = [];
+
+  constructor(private service: PensamentoService) { }
+
+  ngOnInit(): void {
+    this.service.listar().subscribe((listaPensamentos) => {
+      this.listaPensamentos = listaPensamentos
+    })
+
+  }
+}
